7b49d2ba81e7ce7a3936959c1f220d2894e7acf5
bcuser <balbianit@gmail.com>
2021-01-18 14:46:15 -0800

    added css and edited html/js

--- program.js
+++ program.js
@@ -1,3 +1,4 @@
+
 let words = [];
 
 SwapLetters.style.display = "none";
@@ -16,4 +17,21 @@ document.getElementById("start").addEventListener("click", function GetWords()
   };
   EnterWord.style.display = "none";
   SwapLetters.style.display = "block";
+});
+
+document.getElementById("swap").addEventListener("click", function Swap()
+{
+  var swapped = words.map(function(oneWord) {        return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); });
+
+  First.style.display = "none"
+
+  for (let i = 0; i < swapped.length; i++)
+  {
+    let ul = document.createElement('ul');
+    document.getElementById("newDisplay").appendChild(ul);
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML = swapped[i]; 
+  }
+  SwapLetters.style.display = "none";
 });
